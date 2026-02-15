c794a9c134bf3b5b2caa04fa827619e71d3b59cb
gabrielluck2001-art <gabrielluck2001@gmail.com>
2026-02-15 01:26:14 -0300

    Create load-env.js

---
+++ scripts/load-env.js
@@ -0,0 +1,7 @@
+// scripts/load-env.js
+// Arquivo necessário para carregar variáveis de ambiente
+console.log('Carregando variáveis de ambiente...');
+
+// Se precisar carregar um arquivo .env futuramente, descomente:
+// const dotenv = require('dotenv');
+// dotenv.config();
